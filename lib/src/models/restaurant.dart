3e35ccfb43966e3997028bbac807eaac3e57634f
Espera AWO <esperaking81@gmail.com>
2024-04-17 05:48:03 +0100

    feat: add coordinates fields to restaurant

--- lib/src/models/restaurant.dart
+++ lib/src/models/restaurant.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:common/src/models/location.dart';
 import 'package:common/src/models/subscription.dart';
